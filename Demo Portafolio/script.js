d01fa95ff0ada8d41fe0543d1660a472f14d310e
Osmedi <140572467+Osmedi@users.noreply.github.com>
2024-02-19 20:18:52 -0600

    Add files via upload

---
+++ Demo Portafolio/script.js
@@ -0,0 +1,28 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
+
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu() {
+  sidemenu.style.right = "0";
+}
+
+function closemenu() {
+  sidemenu.style.right = "-200px";
+}
+
+// Asignar el evento click al botón de cerrar el menú
+var closebutton = document.getElementById("close-button");
+closebutton.addEventListener("click", closemenu);
